b33734e0b790137672ea88e73b7073b30be45c8f
nandini024 <nandumyakala24@gmail.com>
2025-05-19 10:28:19 +0530

    edited

--- src/Components/receipes/Receipe.jsx
+++ src/Components/receipes/Receipe.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./Receipe.css";
+import "./Receipe.css"; 
+
 
 export default function Receipe({ singleReceipe }) {
   const navigate = useNavigate();
